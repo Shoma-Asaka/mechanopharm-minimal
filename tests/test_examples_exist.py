bd3b79dd51ff4fefe43911eabcc58a3af631f6fb
Shoma-Asaka <st25003@g.u-fukui.ac.jp>
2026-04-21 16:11:04 +0900

    Delete tests directory

--- tests/test_examples_exist.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def test_demo_script_exists():
-    assert Path('examples/demo_minimal.py').exists()
-
-
-def test_notebook_exists():
-    assert Path('notebooks/reproduce_figures.ipynb').exists()
